0478db944790864bada3efccaa298bcd72953efa
josue <jcalderonv05@gmail.com>
2019-11-08 00:37:20 -0600

    Angular template test

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,54 +1,52 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager } from 'ng-jhipster';
+import { Component, OnInit, ElementRef, OnDestroy } from '@angular/core';
 
-import { LoginModalService } from 'app/core/login/login-modal.service';
-import { AccountService } from 'app/core/auth/account.service';
-import { Account } from 'app/core/user/account.model';
+declare let $: any;
 
 @Component({
   selector: 'jhi-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['home.scss']
+  templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  account: Account;
-  authSubscription: Subscription;
-  modalRef: NgbModalRef;
+  test: Date = new Date();
+  private toggleButton: any;
+  private sidebarVisible: boolean;
+  private nativeElement: Node;
 
-  constructor(
-    private accountService: AccountService,
-    private loginModalService: LoginModalService,
-    private eventManager: JhiEventManager
-  ) {}
+  constructor(private element: ElementRef) {
+    this.nativeElement = element.nativeElement;
+    this.sidebarVisible = false;
+  }
 
   ngOnInit() {
-    this.accountService.identity().subscribe((account: Account) => {
-      this.account = account;
-    });
-    this.registerAuthenticationSuccess();
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggle')[0];
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.add('login-page');
+    body.classList.add('off-canvas-sidebar');
+    const card = document.getElementsByClassName('card')[0];
+    setTimeout(function() {
+      // after 1000 ms we add the class animated to the login/register card
+      card.classList.remove('card-hidden');
+    }, 700);
   }
-
-  registerAuthenticationSuccess() {
-    this.authSubscription = this.eventManager.subscribe('authenticationSuccess', message => {
-      this.accountService.identity().subscribe(account => {
-        this.account = account;
-      });
-    });
-  }
-
-  isAuthenticated() {
-    return this.accountService.isAuthenticated();
-  }
-
-  login() {
-    this.modalRef = this.loginModalService.open();
-  }
-
-  ngOnDestroy() {
-    if (this.authSubscription) {
-      this.eventManager.destroy(this.authSubscription);
+  sidebarToggle() {
+    const toggleButton = this.toggleButton;
+    const body = document.getElementsByTagName('body')[0];
+    if (this.sidebarVisible === false) {
+      setTimeout(function() {
+        toggleButton.classList.add('toggled');
+      }, 500);
+      body.classList.add('nav-open');
+      this.sidebarVisible = true;
+    } else {
+      this.toggleButton.classList.remove('toggled');
+      this.sidebarVisible = false;
+      body.classList.remove('nav-open');
     }
+  }
+  ngOnDestroy() {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('login-page');
+    body.classList.remove('off-canvas-sidebar');
   }
 }
